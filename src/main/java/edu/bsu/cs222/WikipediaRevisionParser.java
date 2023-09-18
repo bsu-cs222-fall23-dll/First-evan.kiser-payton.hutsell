1a17ad1eaa2e62b62d9a6dcadc96fea663d9a6ed
paytonh13 <payton.hutsell@gmail.com>
2023-09-18 15:07:18 -0400

    getting Input from the User, in the right file this time

--- src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
@@ -1,26 +1,5 @@
 package edu.bsu.cs222;
 
 
-import java.io.IOException;
-import java.util.Scanner;
-
 public class WikipediaRevisionParser {
-
-    public static void main(String[] args) {
-        WikipediaRevisionReader revisionReader = new WikipediaRevisionReader();
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        try {
-            String timestamp = revisionReader.getLatestRevisionOf(line);
-            System.out.println(timestamp);
-        catch (IOException ioException) {
-            System.err.println("Network connection problem: " + ioException.getMessage());
-            }
-        }
-//        Private String getLatestRevisionOf(String articleTitle) throws IOException {
-//            String urlString = String.format("https://en.wikipedia.org")
-        }
-    }
-
-
 }
--- src/main/java/edu/bsu/cs222/WikipediaRevisionReader.java
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionReader.java
@@ -1,4 +1,20 @@
 package edu.bsu.cs222;
 
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class WikipediaRevisionReader {
+
+    public static void main(String[] args) {
+        WikipediaRevisionReader revisionReader = new WikipediaRevisionReader();
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        try {
+            String timestamp = revisionReader.getLatestRevisionOf(line);
+            System.out.println(timestamp);
+        catch (IOException ioException) {
+                System.err.println("Network connection problem: " + ioException.getMessage());
+            }
+        }
 }
