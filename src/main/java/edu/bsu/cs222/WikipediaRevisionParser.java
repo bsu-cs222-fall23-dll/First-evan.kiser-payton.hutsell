fdef89e7bec3e11301b8e9d7ba0e16371dd5122a
paytonh13 <payton.hutsell@gmail.com>
2023-09-18 15:46:53 -0400

    RevisionParser Input

--- src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
@@ -1,5 +1,13 @@
 package edu.bsu.cs222;
 
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 
+import java.io.IOException;
+import java.io.InputStream;
 public class WikipediaRevisionParser {
+    public String parse(InputStream testDataStream) throws IOException {
+        JSONArray result = (JSONArray) JsonPath.read(testDataStream, "$..timetamp");
+        return result.get(0).toString();
+    }
 }
