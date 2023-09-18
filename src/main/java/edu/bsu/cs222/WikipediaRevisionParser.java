a595df01e5fac3c82bdd6e3048fda7bfd1b3f9dd
Evan Kiser <evan.kiser@bsu.edu>
2023-09-18 16:47:32 -0400

    Preparation for Bug Fixes. 9/18/23

--- src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionParser.java
@@ -1,13 +1,12 @@
 package edu.bsu.cs222;
 
 import com.jayway.jsonpath.JsonPath;
-import net.minidev.json.JSONArray;
 
 import java.io.IOException;
 import java.io.InputStream;
 public class WikipediaRevisionParser {
     public String parse(InputStream testDataStream) throws IOException {
-        JSONArray result = (JSONArray) JsonPath.read(testDataStream, "$..timestamp");
-        return result.get(0).toString();
+        Object result = JsonPath.read(testDataStream, "$..timestamp"); // Should be a JSONArray object
+        return null;
     }
 }
