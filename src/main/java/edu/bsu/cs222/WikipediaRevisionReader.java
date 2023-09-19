325bd65ac40b82210d0b4bf515daef4f1b6583ad
Evan Kiser <evan.kiser@bsu.edu>
2023-09-19 13:46:36 -0400

    Adding dllargent sample code

--- src/main/java/edu/bsu/cs222/WikipediaRevisionReader.java
+++ src/main/java/edu/bsu/cs222/WikipediaRevisionReader.java
@@ -11,31 +11,56 @@ import java.io.InputStream;
 
 public class WikipediaRevisionReader {
 
-    public static void main(String[] args) {
-        WikipediaRevisionReader revisionReader = new WikipediaRevisionReader();
-        Scanner scanner = new Scanner(System.in);
-        String line = scanner.nextLine();
-        try {
-            String timestamp = revisionReader.getLatestRevisionOf(line);
-            System.out.println(timestamp);
-        } catch (IOException ioException) {
-            System.err.println("Network connection problem: " + ioException.getMessage());
-        }
+    public static void main(String[] args) throws IOException {
+        URLConnection connection = connectToWikipedia();
+        String jsonData = readJsonAsStringFrom(connection);
+        printRawJson(jsonData);
     }
 
-    private String getLatestRevisionOf(String articleTitle) throws IOException {
-        String urlString = String.format("https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&list=allrevisions%7Csearch&formatversion=2&arvprop=timestamp%7Cuser&srsearch=Socks", articleTitle);
-        String encodeUrlString = URLEncoder.encode(urlString, Charset.defaultCharset());
-        try {
-            URL url = new URL(encodeUrlString);
-            URLConnection connection = url.openConnection();
-            connection.setRequestProperty("User-Agent", "WikipediaRevisionReader/0.1 (https://youtube.com/paulgestwicki; pvgestwicki@bsu.edu)");
-            InputStream inputStream = connection.getInputStream();
-            WikipediaRevisionParser parser = new WikipediaRevisionParser();
-            String timestamp = parser.parse(inputStream);
-            return timestamp;
-        } catch (MalformedURLException malformedURLException) {
-            throw new RuntimeException(malformedURLException);
-        }
+    private static URLConnection connectToWikipedia() throws IOException {
+        String encodedUrlString = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" +
+                URLEncoder.encode("Zappa", Charset.defaultCharset()) +
+                "&rvprop=timestamp|user&rvlimit=4&redirects";
+        URL url = new URL(encodedUrlString);
+        URLConnection connection = url.openConnection();
+        connection.setRequestProperty("User-Agent",
+                "CS222FirstProject/0.1 (dllargent@bsu.edu)");
+        connection.connect();
+        return connection;
     }
+
+    private static String readJsonAsStringFrom(URLConnection connection) throws IOException {
+        return new String(connection.getInputStream().readAllBytes(), Charset.defaultCharset());
+    }
+
+    private static void printRawJson(String jsonData) {
+        System.out.println(jsonData);
+    }
+//    public static void main(String[] args) {
+//        WikipediaRevisionReader revisionReader = new WikipediaRevisionReader();
+//        Scanner scanner = new Scanner(System.in);
+//        String line = scanner.nextLine();
+//        try {
+//            String timestamp = revisionReader.getLatestRevisionOf(line);
+//            System.out.println(timestamp);
+//        } catch (IOException ioException) {
+//            System.err.println("Network connection problem: " + ioException.getMessage());
+//        }
+//    }
+//
+//    private String getLatestRevisionOf(String articleTitle) throws IOException {
+//        String urlString = String.format("https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&format=json&list=allrevisions%7Csearch&formatversion=2&arvprop=timestamp%7Cuser&srsearch=Socks", articleTitle);
+//        String encodeUrlString = URLEncoder.encode(urlString, Charset.defaultCharset());
+//        try {
+//            URL url = new URL(encodeUrlString);
+//            URLConnection connection = url.openConnection();
+//            connection.setRequestProperty("User-Agent", "WikipediaRevisionReader/0.1 (https://youtube.com/paulgestwicki; pvgestwicki@bsu.edu)");
+//            InputStream inputStream = connection.getInputStream();
+//            WikipediaRevisionParser parser = new WikipediaRevisionParser();
+//            String timestamp = parser.parse(inputStream);
+//            return timestamp;
+//        } catch (MalformedURLException malformedURLException) {
+//            throw new RuntimeException(malformedURLException);
+//        }
+//    }
 }
--- src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
+++ src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
@@ -1,18 +1,38 @@
 package edu.bsu.cs222;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 public class WikipediaRevisionParserTest {
 
-    @Test
-    public void testParse() throws IOException {
-        WikipediaRevisionParser parser = new WikipediaRevisionParser();
-        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
-        String timestamp = parser.parse(testDataStream);
-        Assertions.assertEquals("2021-01-25T00:14:462" , timestamp);
-    }
+        @Test
+        public void testAccessToJsonFile() throws IOException {
+            String jsonData = readSampleFileAsString();
+            Assertions.assertNotNull(jsonData);
+        }
+
+        @Test
+        public void testCountRevisionsWithJsonPath() throws IOException {
+            String jsonData = readSampleFileAsString();
+            JSONArray revisions = getRevisionsFromJson(jsonData);
+            Assertions.assertEquals(4, revisions.size());
+        }
+
+        private String readSampleFileAsString() throws NullPointerException, IOException {
+            InputStream sampleFile = Thread.currentThread().getContextClassLoader()
+                    .getResourceAsStream("sample.json");
+            return new String(Objects.requireNonNull(sampleFile).readAllBytes(), Charset.defaultCharset());
+        }
+
+        private JSONArray getRevisionsFromJson(String jsonData) {
+            return JsonPath.read(jsonData, "$..revisions[*]");
+        }
+
 }
