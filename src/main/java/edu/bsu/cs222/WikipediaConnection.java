b96a91e2566ed6c98383e338a65b3614b1323747
Evan Kiser <evan.kiser@bsu.edu>
2023-09-24 20:57:30 -0400

    Fixing the Production Code in WikipediaConnection

--- src/main/java/edu/bsu/cs222/WikipediaConnection.java
+++ src/main/java/edu/bsu/cs222/WikipediaConnection.java
@@ -7,34 +7,23 @@ import java.net.URLEncoder;
 import java.nio.charset.Charset;
 
 public class WikipediaConnection extends GetUserInput {
-    // Getting User Input
-    static String userSearch = getUserInput();   // Returns the article name the user want to look up
     public static void main(String[] args) throws IOException {
         URLConnection connection = connectToWikipedia();
-        String jsonData = readJsonAsStringFrom(connection);
-        printRawJson(jsonData);
-    }
-
-    public String turningInputToURL() {
-        String articleTitle = URLEncoder.encode(userSearch, Charset.defaultCharset());
-        return String.format("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" + articleTitle
-                + "&rvprop=timestamp|user&rvlimit=13&redirects");
     }
 
     private static URLConnection connectToWikipedia() throws IOException {
-        String encodedUrlString = URLEncoder.encode(userSearch, Charset.defaultCharset()) ;
-        URL url = new URL(encodedUrlString);
+        URL url = new URL(turningInputToURL());
         URLConnection connection = url.openConnection();
         connection.setRequestProperty("User-Agent", "CS222FirstProject/Group G (dllargent@bsu.edu)");
         connection.connect();
         return connection;
     }
 
-    private static String readJsonAsStringFrom(URLConnection connection) throws IOException {
-        return new String(connection.getInputStream().readAllBytes(), Charset.defaultCharset());
+    public static String turningInputToURL() {
+        String userSearch = getUserInput();   // Returns the article name the user want to look up
+        String articleTitle = URLEncoder.encode(userSearch, Charset.defaultCharset());
+        return String.format("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" + articleTitle
+                + "&rvprop=timestamp|user&rvlimit=13&redirects");
     }
 
-    private static void printRawJson(String jsonData) {
-        System.out.println(jsonData);
-    }
 }
