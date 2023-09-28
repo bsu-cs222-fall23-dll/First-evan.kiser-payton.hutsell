f7188551e82e89fe037fae58b63239e4cb2a1b0c
Evan Kiser <evan.kiser@bsu.edu>
2023-09-28 13:39:53 -0400

    v0.2.4 Building a base GUI class

--- src/main/java/edu/bsu/cs222/GUI.java
+++ src/main/java/edu/bsu/cs222/GUI.java
@@ -1,4 +1,33 @@
 package edu.bsu.cs222;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox vbox = new VBox();
+        vbox.getChildren().add(new Label("Wikipedia API Reader"));
+
+        HBox articleName = new HBox(new Label("Enter the Name of the article you want to search up?"));
+        TextField textField = new TextField();
+        articleName.getChildren().add(textField);
+        vbox.getChildren().add(articleName);
+
+        Button button = new Button("Search!");
+        button.setOnAction(event -> {
+
+            System.out.println("\nList of All Revisions: Timestamp - User");
+        });
+        vbox.getChildren().add(button);
+
+        primaryStage.setScene(new Scene(vbox));
+        primaryStage.show();
+    }
 }
