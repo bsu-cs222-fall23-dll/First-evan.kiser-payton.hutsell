c35204959281002d756c130105de7e66a92b0e43
Evan Kiser <evan.kiser@bsu.edu>
2023-10-03 22:00:11 -0400

    v0.2.15 Adding Test

--- src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
+++ src/test/java/edu/bsu/cs222/WikipediaRevisionParserTest.java
@@ -2,18 +2,16 @@ package edu.bsu.cs222;
 
 import edu.bsu.cs222.Exceptions.NoPageExistException;
 import edu.bsu.cs222.Model.WikipediaRevisionParser;
-import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 public class WikipediaRevisionParserTest {
 
     @Test
-    public void testParseWithValidData() throws IOException, NoPageExistException {
+    public void testParseWithValidData() throws NoPageExistException {
         String jsonData = "{\"query\":{\"pages\":{\"123\":{\"revisions\":[{\"id\":1,\"content\":\"Revision 1\"},{\"id\":2,\"content\":\"Revision 2\"}]}}}}";
         InputStream dataStream = new ByteArrayInputStream(jsonData.getBytes());
         WikipediaRevisionParser parser = new WikipediaRevisionParser();
@@ -25,12 +23,12 @@ public class WikipediaRevisionParserTest {
     }
 
     @Test
-    public void testParseWithNoRevisions() throws IOException {
+    public void testParseWithNoRevisions()  {
         String jsonData = "{\"query\":{\"pages\":{\"123\":{\"revisions\":[]}}}}";
         InputStream dataStream = new ByteArrayInputStream(jsonData.getBytes());
         WikipediaRevisionParser parser = new WikipediaRevisionParser();
 
-        String revisions = null;
+        String revisions;
         try {
             revisions = parser.parse(dataStream);
         } catch (NoPageExistException e) {
@@ -41,7 +39,7 @@ public class WikipediaRevisionParserTest {
     }
 
     @Test
-    public void testParseWithInvalidData() throws IOException, NoPageExistException {
+    public void testParseWithInvalidData() throws NoPageExistException {
         String jsonData = "{\"query\":{\"pages\":{\"123\":{\"revisions\":null}}}}";
         InputStream dataStream = new ByteArrayInputStream(jsonData.getBytes());
         WikipediaRevisionParser parser = new WikipediaRevisionParser();
